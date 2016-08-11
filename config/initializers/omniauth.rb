75e91eb35a0f3be8639c571cd6dbbe2c2a2b1f9d
Alex Tarkowski <alex.tarkowski@gmail.com>
2016-08-11 14:27:21 -0400

    add image and js assets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '482303318645754', 'e85d157dba4a74e8150b31bd78507f33', scope: 'email, public_profile', info_fields: 'email,name,first_name,last_name,gender,locale'
+  provider :facebook, 'id', 'secret', scope: 'email, public_profile', info_fields: 'email,name,first_name,last_name,gender,locale'
 end
 
 
